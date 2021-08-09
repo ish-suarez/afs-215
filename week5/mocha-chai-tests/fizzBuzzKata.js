311a44cb582ab0f8553472edfe2309b7fd1ccec3
ish-suarez <ish-suarez@outlook.com>
2021-08-09 01:11:45 -0500

    chai set up

---
+++ week5/mocha-chai-tests/fizzBuzzKata.js
@@ -0,0 +1,8 @@
+module.exports = (num) => {
+    switch(num) {
+        case 1:
+            return `${num}`
+        case 2:
+            return `${num}`
+    }
+}
---
+++ week5/mocha-chai-tests/test/testFizzBuzz.js
@@ -0,0 +1,9 @@
+const expect = require('chai').assert
+const fizzBuzz = require('../fizzBuzzKata')
+
+it('fizzbuzz returns 1', () => {
+    expect.equal(fizzBuzz(1), 1)
+})
+it('fizzbuzz returns 2', () => {
+    expect.equal(fizzBuzz(2), 2)
+}) 
